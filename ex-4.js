9dc862665c29f33c36580b4fd821e4f9ed7c79cb
Ploy <Ploypailin_mt@hotmail.com>
2023-11-20 17:41:59 +0700

    Finished Ex-4

--- ex-4.js
+++ ex-4.js
@@ -3,3 +3,15 @@
 let lightBulbStatus = "On";
 
 //Start coding here
+let message;
+switch (lightBulbStatus) {
+    case "On":
+    message = "Light bulb is On.";
+    break;
+    case "Off":
+    message = "Light bulb is Off.";
+    break;
+    case "Broken":
+    message = "Light bulb is Broken."
+}
+console.log(message)
